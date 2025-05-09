7fe95a4a138b8653f3880cdc3c2900da9abd4bb0
jamel <jamelmurray704@gmail.com>
2025-05-08 20:46:17 -0400

    Enhance HTML structure and styling; add Bootstrap for responsive design

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-//let execrises = ["push-ups", "sit-ups", "squats", "burpees", "plank"];
+let execrises = ["push-ups", "sit-ups", "squats", "burpees", "plank"];
+
+const generatebutton = document.getElementById("generatebutton");
+
+function generate() {
+randomindex = Math.floor(Math.random() * execrises.length);
+const randomexecrise = execrises[randomindex];
+document.querySelector("p").textContent = randomexecrise;
+console.log(randomexecrise);
+}
 
 //function shuffle(execrises) {
     //for (let i = execrises.length - 1; i > 0; i--) {
@@ -18,6 +27,11 @@ function getrandominclusive(min, max) {
 
 }
 console.log(randomsets);
+
+
+
+
+
 
 
 
